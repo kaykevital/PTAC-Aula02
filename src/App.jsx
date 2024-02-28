2151fa5908fc6eeea8e312b199785c90d683d7e0
kaykevital <kayke.macedo@estudante.ifms.edu.br>
2024-02-28 15:04:42 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 export default function App() {
 
   const nome = "João Santos";
+  const novoNome = "Kayke Vital";
   
   return(
     <div>
         <h1>Hello World</h1>
         <p>Desenvolvido por: {nome}</p>
+        <p>EU: {novoNome}</p>
     </div>  
   );
 
