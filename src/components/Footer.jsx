50fc6bf1ceb3d28b4793a45f6daf802324966e2c
kayke <kayke.macedo@estudante.ifms.edu.br>
2024-02-28 22:28:11 -0300

    criando o footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            <h3>Desenvolvedor: Kayke Vital</h3>
+        </footer>
+    )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return(
-            <header>
-                <h2>MC GUIGA</h2>
-            </header>
+        <header>
+            <h2>MC GUIGA</h2>
+        </header>
     );
 
 }
