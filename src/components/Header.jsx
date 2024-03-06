e2675a7f48a607d42d8d53f6999d16ad9af3d34f
kaykevital <kayke.macedo@estudante.ifms.edu.br>
2024-03-06 15:58:55 -0400

    alteraão

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header>
-            <h2>MC GUIGA</h2>
+            <h2>Bem vindo ao site de informações sobre o Kayke</h2>
         </header>
     );
 
