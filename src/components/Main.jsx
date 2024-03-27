21a4c7acbbe3926b8c76addce9a88822b3e7bc75
kaykevital <kayke.macedo@estudante.ifms.edu.br>
2024-03-27 16:05:58 -0400

    att da listadeContato

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,32 @@
 import { useState } from "react"
 
 export default function Main () {
-    const [nome, setNome] = useState("");
-    const [telefone, setTelefone] = useState("");
-    
-    return(
-      <main>
-        <form>
-            <label>Nome:<input type="text" name="nome-contato" id="nome" value={nome} onChange={ (event) => setNome(event.target.value)}/></label>
-            {nome}
-            <label>Telefone:<input type="tel" name="telefone-contato" id="telefone" value={telefone} onChange={ (event) => setTelefone(event.target.value)}/></label>
-            {telefone}
-            <button>Enviar</button>
-        </form>
-      </main>
-    )
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState();
+  const [listaContatos, setListaContatos] = useState([]);
+  const [idade, setIdade] = useState("")
+
+  const registrar = (event) => {
+    event.preventDefault();
+    alert("Deu certo!")
+    setListaContatos([...listaContatos, 
+      {
+        nomeSalvo: nome,
+        telefoneSalvo: telefone,
+        idadeSalvo: idade
+      }
+    ]);
+  }
+  console.table(listaContatos);
+
+  return(
+    <main>
+      <form onSubmit={registrar}>
+          <label htmlFor="nome">Nome:<input type="text" name="nome-contato" id="nome" value={nome} onChange={ (event) => setNome(event.target.value)}/></label>
+          <label>Telefone:<input type="tel" name="telefone-contato" id="telefone" value={telefone} onChange={ (event) => setTelefone(event.target.value)}/></label>
+          <label>Idade: <input type="number" name="idade-contato" id="idade" value={idade} onChange={ (event) => setIdade(event.target.value)}></input></label>       
+          <button>Enviar</button>
+      </form>
+    </main>
+  )
 }
